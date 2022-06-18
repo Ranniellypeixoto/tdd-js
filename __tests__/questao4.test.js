cc933b681b10e10f6a0cfc4ae5bf76cbf9c26a81
Ranniellypeixoto <100246142+Ranniellypeixoto@users.noreply.github.com>
2022-06-18 18:33:54 -0300

    ajuste do teste q4

--- __tests__/questao4.test.js
+++ __tests__/questao4.test.js
@@ -1,26 +1,25 @@
 const pagarConta = require('../questoes/questao4')
 
 test("Pagamento de conta saldo positivo", () => {
+    nome = "Fulano"
     saldoEmConta = 500
     agua = 30.00
     energia = 50.00
     lanche = 26.00
-    despesas = (agua + energia + lanche)
-    saldo = (saldoEmConta - despesas)
-    
+    saldo = 394
 
-    resultado = pagarConta(saldo)
-    expect(resultado).toBe(`${nome}, conta paga com sucesso, seu saldo é R$${saldo}`);
+    resultado = pagarConta(nome, agua, energia, lanche, saldoEmConta)
+    expect(resultado).toBe(`${nome}, conta paga com sucesso, seu saldo é R$ ${saldo}`);
 })
 
 test("Pagamento de conta saldo negativo", () => {
+    nome = "Fulano"
     saldoEmConta = 30
     agua = 30.00
     energia = 50.00
     lanche = 26.00
-    const despesas = (agua + energia + lanche)
-    const saldo = (saldoEmConta - despesas)
+    saldo = -76
     
-    resultado = pagarConta(saldo)
-    expect(resultado).toBe(`${nome}, seu saldo é insuficiente, falta R$${saldo} para conseguir efetuar o pagamento`);
+    resultado = pagarConta(nome, agua, energia, lanche, saldoEmConta)
+    expect(resultado).toBe(`${nome}, seu saldo é insuficiente, falta R$ ${saldo} para conseguir efetuar o pagamento`);
 })
--- questoes/questao4.js
+++ questoes/questao4.js
@@ -1,6 +1,13 @@
-//Questão 4: Fulano da Silva Sauro é correntista do Banco do Brasil, ele mora na Rua da Lama, no estado XPTO e na cidade XYZ. O saldo da conta bancária de Fulano é de 100 reais e ele pagou a conta de água no valor de R$ 30,00, a conta de energia no valor de R$ 50,00 e fez um lanche que custou R$ 26,00. Implemente um algoritmo que represente o cenário de Fulano e exiba uma mensagem “Conseguiu pagar a conta na lanchonete” ou “Saldo insuficiente, falta R$ ?,00 para pagar a conta”
+/*Questão 4: Fulano da Silva Sauro é correntista do Banco do 
+Brasil, ele mora na Rua da Lama, no estado XPTO e na cidade XYZ.
+O saldo da conta bancária de Fulano é de 100 reais e ele pagou a
+conta de água no valor de R$ 30,00, a conta de energia no valor
+de R$ 50,00 e fez um lanche que custou R$ 26,00. Implemente
+um algoritmo que represente o cenário de Fulano e exiba uma 
+mensagem “Conseguiu pagar a conta na lanchonete” ou “Saldo 
+insuficiente, falta R$ ?,00 para pagar a conta”
+*/
 
-nome = "Fulano da Silva Sauro";
 /*endereco = {
   rua: "Rua da Lama",
   estado: "XPTO", 
@@ -8,17 +15,15 @@ nome = "Fulano da Silva Sauro";
 }*/
 //saldoEmConta = 500
 
-function pagarConta(){
-agua = 30.00
-energia = 50.00
-lanche = 26.00
-const despesas = (agua + energia + lanche)
-const saldo = (saldoEmConta - despesas)
+function pagarConta(nome, agua, energia, lanche, saldoEmConta) {
+  const despesas = (agua + energia + lanche)
+  const saldo = (saldoEmConta - despesas)
 
-if (saldo >= 1) {
-  return `${nome}, conta paga com sucesso, seu saldo é R$${saldo}`
-} else {
-  return  `${nome}, seu saldo é insuficiente, falta R$${saldo} para conseguir efetuar o pagamento`
+  if (saldo >= 1) {
+    return `${nome}, conta paga com sucesso, seu saldo é R$ ${saldo}`
+  } else {
+    return `${nome}, seu saldo é insuficiente, falta R$ ${saldo} para conseguir efetuar o pagamento`
+  }
 }
-}
+
 module.exports = pagarConta;
