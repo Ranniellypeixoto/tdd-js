cb6c1df8bd872d318b13990c546a256e1235790a
Yonatha <yonathalmeida@gmail.com>
2022-07-07 21:50:15 -0300

    Adição do teste de quantidade total de produtos comprados na Q5

--- __tests__/questao5.test.js
+++ __tests__/questao5.test.js
@@ -7,53 +7,38 @@ Implemente um algoritmo que mostre quanto cada um gastou
 
 const Compra = require("../questoes/questao5")
 
+const compra = new Compra();
+const mockDimitri = [
+    { nome: "banana", qtd: 12, preco: 2.50 },
+    { nome: "abacaxi", qtd: 1, preco: 2.78 },
+    { nome: "maçã", qtd: 5, preco: 6.94 },
+]
+
+const mockMonique = [
+    { nome: "banana", qtd: 10, preco: 2.00 },
+    { nome: "tomates", qtd: 6, preco: 5.00 },
+    { nome: "abacaxi", qtd: 2, preco: 5.00 },
+    { nome: "morango", qtd: 10, preco: 10.00 },
+]
+
+
 test('Calcular total sacola Dimitri', () => {
-    mockDimitri = [
-        { nome: "banana", qtd: 12, preco: 2.50 },
-        { nome: "abacaxi", qtd: 1, preco: 2.78 },
-        { nome: "maçã", qtd: 5, preco: 6.94 },
-    ]
-
-    const compra = new Compra();
     totalDrimitri = compra.calcularTotal(mockDimitri)
-
     expect(totalDrimitri).toBe("R$ 12,22")
 });
 
 test('Calcular total sacola Monique', () => {
-    mockMonique = [
-        { nome: "banana", qtd: 10, preco: 2.00 },
-        { nome: "tomates", qtd: 6, preco: 5.00 },
-        { nome: "abacaxi", qtd: 2, preco: 5.00 },
-        { nome: "morango", qtd: 10, preco: 10.00 },
-    ]
-
-    const compra = new Compra();
     totalMonique = compra.calcularTotal(mockMonique)
-
     expect(totalMonique).toBe("R$ 22,00")
 });
 
 test('Calcular total compras do casal', () => {
-    mockDimitri = [
-        { nome: "banana", qtd: 12, preco: 2.50 },
-        { nome: "abacaxi", qtd: 1, preco: 2.78 },
-        { nome: "maçã", qtd: 5, preco: 6.94 },
-    ]
-
-    mockMonique = [
-        { nome: "banana", qtd: 10, preco: 2.00 },
-        { nome: "tomates", qtd: 6, preco: 5.00 },
-        { nome: "abacaxi", qtd: 2, preco: 5.00 },
-        { nome: "morango", qtd: 10, preco: 10.00 },
-    ]
-
     produtosDoCasal = mockDimitri.concat(mockMonique)
-
-    const compra = new Compra();
-    totalDrimitri = compra.calcularTotal(produtosDoCasal)
-
+    totalDrimitri = compra.calcularTotalv2(produtosDoCasal)
     expect(totalDrimitri).toBe("R$ 34,22")
 });
 
-
+test('Quantidade total produtos comprados', () => {
+    produtosDoCasal = mockDimitri.concat(mockMonique)
+    expect(compra.calcularTotalProdutos(produtosDoCasal)).toEqual(46)
+});
